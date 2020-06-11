185973c2d26207e796e57482d052b9fed41e35ce
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2020-06-11 12:56:30 +0200

    basic commands

--- game.js
+++ game.js
@@ -2,17 +2,50 @@
 class Game {
 
   constructor() {
-    this.hp = 3
-    this.sp = 2
-    this.mp = 1
+    this.hp_max = 3
+    this.sp_max = 2
+    this.mp_max = 1
 
-    this.msg = "WELCOME"
+    this.loc = "A GREEN VALLEY, FLANKED BY MOUNTAINS. THROUGH IT FLOWS A MUDDY STREAM."
+
+    this.rest()
   }
 
   exec(cmd) {
-    this.hp -= 1
-    this.msg = "HIT"
+    if (cmd == "👣") {
+      return "YOU WALK"
+    }
+    
+    if (cmd == "👁") {
+      return "YOU SEE " + this.loc
+    }
+
+    if (cmd == "🗡") {
+      self.hp -= 1
+      return "YOU CUT YOURSELF"
+    }
+
+    if (cmd == "💤") {
+      this.rest()
+      return "YOU SLEEP ... WHEN YOU WAKE UP, YOU FEEL REPLENISHED"
+    }
+
+    return "NOTHING HAPPENS"
   }
 
+  rest() {
+    this.hp = this.hp_max
+    this.sp = this.sp_max
+    this.mp = this.mp_max
+  }
+
+}
+
+class Creature {
+
+  constructor(hp, dmg) {
+    this.hp = hp
+    this.dmg = dmg
+  }
 }
 
