0868690b499760140be80d8b4781a0d6e232803d
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2020-06-15 00:01:13 +0200

    spell system

---
+++ data.js
@@ -0,0 +1,18 @@
+creatureData = {
+	"player": [5, 5, 5, "YOU", "YOU", []],
+	"rat": [2, 0, 0, "A RAT", "THE RAT", ["bite"]]
+}
+
+cast = {
+	bite(c, t) {
+		t.lostHealth += 1
+		return c.name + " BITES YOU"
+	},
+	hit(c, t) {
+		t.lostHealth += 1
+		return "YOU HIT " + t.name
+	},
+	idle(c, t) {
+		return c.name " DOES NOTHING"
+	}
+}
--- game.js
+++ game.js
@@ -1,60 +1,107 @@
 class Game {
 
-  constructor() {
-    this.level = 1
-
-    this.health = 10
-    this.wisdom = 10
-	this.power = 10
-
-    this.spellBook = new SpellBook()
-  }
-
-  exec(cmd) {
-  	if (cmd == "START") {
-		return nextEncounter()
+	constructor() {
+		this.player = new Creature("player")
+		this.generateSpellbook()
+		this.nextCreature()
 	}
 
+	generateSpellbook() {
+		this.spellbook = {
+			"hit": "hit"
+		}
+	}
 
+	intro() {
+		var msgs = []
+		msgs.append("THE MAGIC IN OUR WORLD HAS GROWN WEAK. THERE USED TO BE WORDS, POWERFUL WORDS - WORDS THAT WOULD FORGE EMPIRES AND DEVASTATE LANDSCAPES. BUT ALMOST ALL OF THEM HAVE BEEN LOST. THE ONES WE REMEMBER ARE SIMPLE AND XXX. TODO: LIST? YOUR SEARCH FOR ... HAS FINALLY BROUGHT YOU TO THE VALLEY OF MISTS, A PLACE THAT IS FULL OF SECRETS AND RELICTS OF ANCIENT TIMES ...")
+		msgs.append("")
+		msgs.append("AS YOU ENTER THE VALLEY, YOU ENCOUNTER " + this.creature.undefName + "!")
+		msgs.append("CHOOSE YOUR WORDS")
+		return msgs
+	}
 
-    return "NOTHING HAPPENS."
-  }
+	exec(cmd) {
+		var msgs = []
+		// train health from being hit
+		// train spell school from using spells
+		// train power with every encounter
 
-  nextEncounter() {
-    this.hp = this.health * 5
-  	this.creature = new Creature()
-	return "AS YOU PROCEED, YOU ENCOUNTER " + this.creature.name + "! WHAT DO YOU DO?"
-  }
+		const playerSpellId = this.spellBook[cmd]
+		if (playerSpellId) {
+			const msg = cast[playerSpellId](this.player, this.creature)
+			msgs.append(msg)
+		} else {
+			msgs.append("NOTHING HAPPENS")
+		}
 
-  fight(cmd) {
+		if (this.creature.isAlive()) {
 
-	// train the skill you used
-  }
+			const creatureSpellId = this.creature.nextSpellId()
+			const msg = cast[creatureSpellId](this.creature, this.player)
+			msgs.append(msg)
 
+			if (this.player.isAlive()) {
+				msgs.append("CHOOSE YOUR NEXT WORDS")
+			} else {
+				msgs.append("YOU DIE")
+				this.spellBook = {}
+			}
 
-  learn(cmd) {
+		} else {
 
-  }
+			msgs.append(this.creature.name + " DIES")
+			if (this.lostHealth > 0) {
+				msgs.append("YOU REST UNTIL YOUR WOUNDS HAVE FULLY HEALED")
+				this.lostHealth = 0
+			}
 
-  levelUp() {
-    this.level += 1
-    return "YOU REACHED LEVEL " + this.level + ". WHICH STAT DO YOU WANT TO INCREASE?"
-  }
+			const creatureId = selectNextCreatureId()
+			this.creature = new Creature(creatureId)
+			msgs.append("")
+			msgs.append("AS YOU PROCEED, YOU ENCOUNTER " + this.creature.undefName + "!")
+			msgs.append("CHOOSE YOUR WORDS")
+		}
+		
 
-  tellStats() {
-  	return "HEALTH " + this.health ", WISDOM
-  }
+		return msgs
+	}
 
-  restore() {
-  }
+	selectNextCreatureId() {
+		// TODO: implement
+		return "rat"
+	}
 
 }
 
+
 class Creature {
 
-  constructor(hp, dmg) {
-    this.hp = hp
-    this.dmg = dmg
-  }
+	constructor(id) {
+		const data = creatureData[id]
+
+		this.health = data[0]
+		this.wisdom = data[1]
+		this.power = data[2]
+
+		this.undefName = data[3]
+		this.name = data[4]
+		this.spellIds = data[5]
+
+		this,lostHealth = 0
+	}
+
+	isAlive() {
+		return this.lostHealth < this.health
+	}
+
+	nextSpellId() {
+		if (!this.isAlive()) {
+			return
+		}
+
+		// TODO: random spell ID
+		return this.spellIds[0]
+	}
 }
 
